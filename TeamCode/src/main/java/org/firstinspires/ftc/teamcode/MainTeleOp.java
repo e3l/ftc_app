ae62b9e0d84c862a66f15b9463b9da823d35fdf5
VisheshKG <HeyVishesh@gmail.com>
2018-10-14 16:53:24 -0700

    Tele op intake (#5)
    * added jointMotor and intakeServer, removed two intake motors, set ZeroPowerBehavior Break, added jointMotor to intake method, and fixed intakeServo to the intake method, changed slow to protected access type
    
    * Created constants for joint motor and modified joint motor part of intake()
    
    * Deleted commented intakeMotors
    
    * Deleted some of the commmented code (useless stufff)
    
    * switched bumpers for joint motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.lynx.LynxI2cColorRangeSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -14,14 +15,19 @@ public class MainTeleOp extends LinearOpMode{
     protected DcMotor leftDriveMotor;
     protected DcMotor intakeSlideMotor;
     protected DcMotor liftSlideMotor;
-    protected DcMotor intake0Motor;
-    protected DcMotor intake1Motor;
+    protected DcMotor jointMotor;
+
 
     // Servos
     protected Servo depositServo;
+    protected CRServo intakeServo;
 
     // Color sensors
     protected LynxI2cColorRangeSensor sampleSensor;
+
+    //constants
+    public static final int JOINT_FOLDED = 0; //TODO
+    public static final int JOINT_EXTENDED = 0; //TODO
 
     private void initOpMode() {
         //initialize all the motors
@@ -29,11 +35,20 @@ public class MainTeleOp extends LinearOpMode{
         leftDriveMotor = hardwareMap.get(DcMotor.class, "leftDriveMotor");
         intakeSlideMotor = hardwareMap.get(DcMotor.class, "intakeSlideMotor");
         liftSlideMotor = hardwareMap.get(DcMotor.class, "liftSlideMotor");
-        intake0Motor = hardwareMap.get(DcMotor.class, "intake0Motor");
-        intake1Motor = hardwareMap.get(DcMotor.class, "intake1Motor");
+        jointMotor = hardwareMap.get(DcMotor.class, "jointMotor");
+
+        jointMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        jointMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        rightDriveMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftDriveMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intakeSlideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftSlideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        jointMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         //initialize the servos
         depositServo = hardwareMap.get(Servo.class, "depositServo");
+        intakeServo = hardwareMap.get(CRServo.class, "intakeServo");
 
         // Sensors initialization
         sampleSensor = hardwareMap.get(LynxI2cColorRangeSensor.class, "sampleSensor");
@@ -54,7 +69,7 @@ public class MainTeleOp extends LinearOpMode{
     }
 
     // slow variable to allow for 'slowmode' - allowing the robot to go slower.
-    double slow = 1.66;
+    protected double slow = 1.66;
 
 //controller 1
     private void drive() {
@@ -76,14 +91,20 @@ public class MainTeleOp extends LinearOpMode{
 
 //controller 2
     private void intake() {
-        //Skeleton is there, but I don't know how the args in the setPower() function work so that's up to someone else
-        /*if (gamepad2.left_stick_y != 0) {
-            intakeSlideMotor.setPower(/*gamepad2.left_stick_y / slow * 2);
+       if (gamepad2.left_stick_y != 0) {
+            intakeSlideMotor.setPower(gamepad2.left_stick_y);
+        }
+
+        if (gamepad2.right_bumper) {
+            jointMotor.setTargetPosition(JOINT_EXTENDED);
+        }
+        else if (gamepad2.left_bumper) {
+            jointMotor.setTargetPosition(JOINT_FOLDED);
+        }
 
         if (gamepad2.right_trigger != 0) {
-            intake0Motor.setPower(/*gamepad1.right_trigger / slow * 2);
-            intake1Motor.setPower(/*gamepad1.right_trigger / slow * 2);*/
-
+            intakeServo.setPower(gamepad2.right_trigger);
+        }
     }
 
 //controller 2
