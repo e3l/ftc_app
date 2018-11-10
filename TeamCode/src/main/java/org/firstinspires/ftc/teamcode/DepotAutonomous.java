8a27d4ada5a0627f1b9f9860b03e0004ddc0778e
Vishesh Goyal <visheshgoyal@Admins-MBP.hsd1.ca.comcast.net>
2018-11-09 20:44:21 -0800

    Crated depoAuto class and pasted in the craterAuto code to use as a template currently commented out due to some changes made to the master class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DepotAutonomous.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+public class DepotAutonomous extends MainAutonomous {
+
+
+    public void runOpMode() {
+        //lower the robot from the lander
+/*        lower();
+
+        //move to the crater sample and get in position
+        turn(LANDER_TO_NS_WALL_DEGREES);
+        moveInch(LANDER_TO_SAMPLE_START_INCHES);
+        turn(NS_WALL_TO_SAMPLE_START_DEGREES);
+
+        //TODO: do the sample stuff
+
+        //move to the other sample
+        turn(SAMPLE_END_TO_PARALLEL_WALL_DEGREES);
+        moveInch(SAMPLE_END_TO_SAMPLE_START_INCHES);
+        turn(PARALLEL_WALL_TO_SAMPLE_START_DEGREES);
+
+        //TODO: do the sample stuff 2.0
+
+        //go to depot to place team market
+        turn(TURN_AROUND);
+        moveInch(SAMPLE_END_TO_EW_WALL_CENTER_INCHES);
+        turn(EW_WALL_CENTER_TO_DEPOT_DEGREES);
+        moveInch(EW_WALL_CENTER_TO_DEPOT_INCHES);
+        //TODO: team marker
+
+        //go and partially park in crater
+        turn(TURN_AROUND);
+        moveInch(DEPOT_TO_CRATER_INCHES);
+        jointPosition("extended");
+
+
+
+*/
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainAutonomous.java
@@ -37,8 +37,19 @@ public abstract class MainAutonomous extends LinearOpMode {
     protected static final int JOINT_EXTENDED = 0; // TODO: Find position
     protected static final int JOINT_FOLDED = 0; // TODO: Find position
 
-    // Distance Constants
+    // More Constants - Field Numbers
     protected static final int SAMPLE_LENGTH = 54;
+    protected static final int LANDER_TO_NS_WALL_DEGREES = -135;
+    protected static final int LANDER_TO_SAMPLE_START_INCHES = 36;
+    protected static final int NS_WALL_TO_SAMPLE_START_DEGREES = -135;
+    protected static final int SAMPLE_END_TO_PARALLEL_WALL_DEGREES = 45;
+    protected static final int SAMPLE_END_TO_SAMPLE_START_INCHES = 48;
+    protected static final int PARALLEL_WALL_TO_SAMPLE_START_DEGREES = -45;
+    protected static final int TURN_AROUND = 180;
+    protected static final int SAMPLE_END_TO_EW_WALL_CENTER_INCHES = 60;
+    protected static final int EW_WALL_CENTER_TO_DEPOT_DEGREES = -135;
+    protected static final int EW_WALL_CENTER_TO_DEPOT_INCHES = 48;
+    protected static final int DEPOT_TO_CRATER_INCHES = 72;
 
     protected void initOpMode() {
         driveTrainMotorLeft = hardwareMap.get(DcMotor.class, "driveTrainMotorLeft");
