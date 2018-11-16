9ffea63198ba5eb4f2cb23f633f3dbcb990137d2
Vishesh Goyal <visheshgoyal@Admins-MacBook-Pro.local>
2018-11-16 12:28:07 -0800

    in the middle of working on depoAuto but my conmputer is gonna die so I need to commit it before it loses the code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DepotAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DepotAutonomous.java
@@ -5,37 +5,37 @@ public class DepotAutonomous extends MainAutonomous {
 
     public void runOpMode() {
         //lower the robot from the lander
-/*        lower();
+        lower();
 
         //move to the crater sample and get in position
-        turn(LANDER_TO_NS_WALL_DEGREES);
-        moveInch(LANDER_TO_SAMPLE_START_INCHES);
-        turn(NS_WALL_TO_SAMPLE_START_DEGREES);
+        turn(LANDER_TO_NS_WALL_DEGREES, 0.4);
+        moveInch(LANDER_TO_SAMPLE_START_INCHES, 0.4);
+        turn(NS_WALL_TO_SAMPLE_START_DEGREES, 0.4);
 
         //TODO: do the sample stuff
 
         //move to the other sample
-        turn(SAMPLE_END_TO_PARALLEL_WALL_DEGREES);
-        moveInch(SAMPLE_END_TO_SAMPLE_START_INCHES);
-        turn(PARALLEL_WALL_TO_SAMPLE_START_DEGREES);
+        turn(SAMPLE_END_TO_PARALLEL_WALL_DEGREES, 0.4);
+        moveInch(SAMPLE_END_TO_SAMPLE_START_INCHES, 0.4);
+        turn(PARALLEL_WALL_TO_SAMPLE_START_DEGREES, 0.4);
 
         //TODO: do the sample stuff 2.0
 
         //go to depot to place team market
-        turn(TURN_AROUND);
-        moveInch(SAMPLE_END_TO_EW_WALL_CENTER_INCHES);
-        turn(EW_WALL_CENTER_TO_DEPOT_DEGREES);
-        moveInch(EW_WALL_CENTER_TO_DEPOT_INCHES);
+        turn(TURN_AROUND, 0.4);
+        moveInch(SAMPLE_END_TO_EW_WALL_CENTER_INCHES, 0.4);
+        turn(EW_WALL_CENTER_TO_DEPOT_DEGREES, 0.4);
+        moveInch(EW_WALL_CENTER_TO_DEPOT_INCHES, 0.4);
         //TODO: team marker
 
         //go and partially park in crater
-        turn(TURN_AROUND);
-        moveInch(DEPOT_TO_CRATER_INCHES);
+        turn(TURN_AROUND, 0.4);
+        moveInch(DEPOT_TO_CRATER_INCHES, 0.4);
         jointPosition("extended");
 
 
 
-*/
+
 
     }
 }
